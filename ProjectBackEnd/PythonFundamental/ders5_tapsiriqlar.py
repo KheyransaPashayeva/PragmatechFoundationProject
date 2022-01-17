a665588ea84564c3214091f37bf1efefe930e371
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2022-01-17 16:42:05 +0400

    python ders tapsiriqlarin etdim

--- ProjectBackEnd/PythonFundamental/ders5_tapsiriqlar.py
+++ ProjectBackEnd/PythonFundamental/ders5_tapsiriqlar.py
@@ -28,6 +28,22 @@
 # print(multiply(my_list))
 # print(multiply(sample_list)) // 5) Sample List : (8, 2, 3, -1, 7) Expected Output : -336
 # ===========================================================================================================
+#6) Write a function called returnDay. This function takes in one parameter ( a number from 1-7) and returns
+#  the day of the week ( 1 is Sunday, 2 is Monday etc.). If the number is less than 1 or greater than 7,
+#  the function should return None.
+# typeday = int(input('eded daxil edin: '))
+# day = ['Monday','Tuesday ','Wednesday ','Thursday  ','Friday  ','Saturday ','Sunday']
+# my_list =[1,2,3,4,5,6,7]
+# def returnDay():
+#     for i in my_list:
+#         if typeday >=1 and typeday <=7:
+#             day_index =my_list.index(typeday)
+#             for j in range(len(day)):
+#              if typeday in my_list:
+#               return f'{typeday}-ci gun {day[day_index]}-dir'
+#     return 'Daxil etdiyiniz eded 1den kicik 7den boyuk olmamalidir'
+# print(returnDay())          
+# ============================================================================================================
 #7) 'admin' adı da daxil olmaqla beş və ya daha çox istifadəçi adının siyahısını tərtib edin.
 #  Təsəvvür edin ki, hər bir istifadəçi vebsayta daxil olduqdan sonra ona salam yazacaq kod yazırsınız.
 #  Siyahıda dövr edin və hər bir istifadəçi üçün salamı çap edin: • İstifadəçi adı 'admin'dirsə, 
@@ -41,6 +57,63 @@
 #      else:
 #         print(' Hello admin, would you like to see a status report?') 
 # salamla(username)
+# ====================================================================================================================
+# 8)dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
+#  Verilen dictionary-e esasen asgidaki suallari cavablandirmaq ucun ekrana sualin cavabiniz yazdirin. 
+# Bunun ucun userden input alin. Eger user “born”, “when” sozlerini daxil etse program texmin etsin ki user 
+# ne sorushmaq isteyir. Meselen born ve when sozleri varsa cumlede user cox guman ki “When was Plato born?” 
+# sualina cavab axtarir. Proqram o sozleri gorub sorushsun ki, “Maybe did you mean “When was Plato born?” “.
+#  Bu suali sorushduqda yes ve no secimleri verin. Eger yes yazsa dictionarydeki datadan istifade ederek
+#  Platonun doguldugu ili usere gosterin.Meselen country daxil etse proqram texmin etsin ki user platonun 
+# doguldugu yeri axtarir ve siz de ele proqram yazin ki ona uygun cavab qaytarin. 
+# Eger mentiqsiz soz daxil edilse not found verin ekrana.
+# dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
+# word = input('writing thing:')
+# def sual_cavab(x):
+#     for i in x:
+#         if word =='born' or word =='when':
+#             print('Maybe did you mean “When was Plato born?')
+#             yes_no =input('answer:')
+#             if yes_no =='yes':
+#              answer= dict.get('born')
+#              return answer
+#             else:
+#              break
+#         if word == 'country' or word == 'where':
+#               print('Maybe did you mean “Where was Plato born?')
+#               yes_no =input('answer:')
+#               if yes_no =='yes':
+#                    answer= dict.get('country')
+#                    return answer
+#               else:
+#                   break
+#         if word == 'name' or word == 'who':
+#               print('Maybe did you mean “What is his name?')
+#               yes_no =input('answer:')
+#               if yes_no =='yes':
+#                    answer= dict.get('name')
+#                    return answer
+#               else:
+#                   break
+#         if word == 'teacher' or word == 'subject':
+#               print('Maybe did you mean "Which is subject teacher?"')
+#               yes_no =input('answer:')
+#               if yes_no =='yes':
+#                    answer= dict.get('teacher')
+#                    return answer
+#               else:
+#                   break
+#         if word == 'student':
+#               print('Maybe did you mean "Who is student?"')
+#               yes_no =input('answer:')
+#               if yes_no =='yes':
+#                    answer= dict.get('teacher')
+#                    return answer
+#               else:
+#                   break  
+#     if word not in dict:
+#          return 'Not found'
+# print(sual_cavab(dict))             
 # ===========================================================================================================
 #9) len() funksiyasini ozunuz yazmaga calishin.
 # my_list = [6,9,0,7,3,11,23,6,43,67]
@@ -60,3 +133,15 @@
 #            return True
 #     return False
 # print(is_first_last_true(numbers))
+#11) Funksiya yazin parameter olaraq list,ve dict qebul etsin. Funksiya yoxlamalidi listin icindeki 
+# reqemler dictioneryde yoxdusa onlari silmelidi ve sonda listi return etmelidi.
+#  (mes : [27,22,34,44],{"tural": 27,"soltan": 22} funksiyaya gonderirsen o sene [27,22] qaytarir.
+# my_parametr =[[27,8,22,34,11,44],{"tural":27,"soltan":22,"melik":31,"eli":11}]
+# def kesisme(x):
+#     my_list =[]
+#     for i in x[0]:
+#       for j in x[1].values():
+#         if i == j: 
+#           my_list.append(j)
+#     print(my_list)
+# kesisme(my_parametr)           
