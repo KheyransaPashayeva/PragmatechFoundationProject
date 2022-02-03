26942290a4b4c9a0cf7b876aff65f2cd1cfd6b80
Kheyransa Pashayeva <78317997+KheyransaPashayeva@users.noreply.github.com>
2022-02-03 18:22:48 +0400

    Update dict.py

--- FinalTasks/part03/dict.py
+++ FinalTasks/part03/dict.py
@@ -14,7 +14,7 @@ def FindCity(cityname):
 def FindCountry(countryname):
   # olke adi daxil edildiyi zaman o olkeye aid olan seherlerin adlarini ekranda gostersin
   if countryname in olkeler.keys():
-   seherler = olkeler.get(countryname)
+   seherler = olkeler.get(countryname) #daxil edilen olkenin valuerin secdim
    print(f'{countryname} olkesinde olan seher adlari:' ,seherler)
   pass
 
@@ -22,13 +22,13 @@ def CityCountMax():
   # ek cox sehere sahib olan olkeni gostersin
  Olkeler = list(olkeler.keys())
  seherler = list(olkeler.values())
- max_seherili_olke=[]
- max_sayi=0
+ seherlerin_sayi=[]
+ max_seher=0
  for j in range(len(seherler)):
-   max_seherili_olke.append(len(seherler[j]))
-   max_sayi=max(max_seherili_olke)
-   indeks = max_seherili_olke.index(max_sayi)
- print('En cox sehere sahib olan olke:',Olkeler[indeks])
+   seherlerin_sayi.append(len(seherler[j])) #ilk once her olkenin seher saylarini bir liste elave etdim
+   max_seher=max(seherlerin_sayi)   #en boyuk ededi tapdim
+   indeks = seherlerin_sayi.index(max_seher) # en boyuk ededin durdugu indeksi tapdim
+ print('En cox sehere sahib olan olke:',Olkeler[indeks])  # hemin indeksli olkeler listindeki olkeni capa verdim
 pass
 
 def CountAllCities():
@@ -36,7 +36,7 @@ def CountAllCities():
   say=0
   seher_list=list(olkeler.values())
   for i in range(len(olkeler.values())):
-    say += len(seher_list[i])
+    say += len(seher_list[i])       # her defe say uzerine seherlerin sayini geldim
   print('Olkeler siyahisin da olan butun seherlerin sayi: ',say)
   pass
 FindCity(olke_seher)
