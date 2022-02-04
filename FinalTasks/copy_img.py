ad6247809d65cd429ff86c20d0d58baa4a1d8897
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2022-02-04 21:59:41 +0400

    final tasklarina elave

---
+++ FinalTasks/copy_img.py
@@ -0,0 +1,5 @@
+import shutil
+from shutil import copyfile
+sekil1_file='C:/Users/TARIEL/Desktop/PragmatechFoundationProject-master/FinalTasks/sekil1_file/img6.jpg'
+sekil2_file ='C:/Users/TARIEL/Desktop/PragmatechFoundationProject-master/FinalTasks/sekil2_file'
+copyfile(sekil1_file, sekil2_file)
--- FinalTasks/part02_sual10.py
+++ FinalTasks/part02_sual10.py
@@ -10,7 +10,7 @@
 # -parol daxil etdikden sonra parol sifrelenmis sekilde sisteme dusmelidir.Bunun ucun encript arasdirmasini ede bilersiz
 # -ana menuye qayitmaq yuxaridaki menuyunun gorunmesi ,proqramdan cixmaq ise umumi proqramin dayanmasi menasina gelir
 def ana_menu():
- return('''Qeydiyyatdan kecmek ucun [1] yazin:\nSisteme daxil olmaq ucun [2] yazin:\n
+    return('''Qeydiyyatdan kecmek ucun [1] yazin:\nSisteme daxil olmaq ucun [2] yazin:\n
  Ana menuye qayitmaq ucun [3] yazin:\nSistemden cixmaq ucun [4] yazin:''')
 print(ana_menu())
 secim = int(input('Seciminizi daxil edin: '))
@@ -19,28 +19,31 @@ class Istifadeci:
     def __init__(self,ad,parol):
         self.ad=ad
         self.parol=parol
-        istifadeciler.append(self)   #liste elave etmek ucun
-        def istifadeci_data_goster(self):
-            print(f'{self.ad} siz qeydiyyatdan kecmisiniz')
+        istifadeciler.append(self.ad)   #liste elave etmek ucun
+        
+    def istifadeci_data_goster(self):
+        print(f'{self.ad} siz artiq qeydiyyatdan kecmisiniz')
 
 while True:
-  if secim == 1:
+    if secim == 1:
         ad=input('-Istifadeci adiniz: ')
         parol=input('-Parolunuz: ')
         istifadeci = Istifadeci(ad, parol)  
         print('ugurla qeydiyyatdan kecdiniz!')
         secim = int(input('Seciminizi daxil edin: '))     
-  if secim==2:
-       adiniz = input('Adinizi daxil edin:')
-       for adiniz in istifadeciler:
+    if secim==2:
+        ad=input('-Istifadeci adiniz: ')
+        parol=input('-Parolunuz: ')
+        istifadeci = Istifadeci(ad,parol)
+        if istifadeci.ad in istifadeciler:
            istifadeci.istifadeci_data_goster()
-           if adiniz not in istifadeciler:
-                print('Qeydiyyatdan kecmemisiniz!')
-       secim = int(input('Seciminizi daxil edin: '))
-  if secim == 3:
+        elif istifadeci not in istifadeciler:
+           print('Qeydiyyatdan kecmemisiniz!')
+        secim = int(input('Seciminizi daxil edin: '))
+    if secim == 3:
         print(ana_menu()) 
         secim = int(input('Seciminizi daxil edin: '))
 
-  if secim ==4:
-   break
+    if secim ==4:
+       break
 
