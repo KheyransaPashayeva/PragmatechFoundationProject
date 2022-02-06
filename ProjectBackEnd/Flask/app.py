ec7f2e91137e8a24706e85b08636322b7318f65a
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2022-02-06 23:14:03 +0400

    final tasklardan isledim

--- ProjectBackEnd/Flask/app.py
+++ ProjectBackEnd/Flask/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
-
+from flask import Flask,render_template,redirect,request,url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
+app.config['SQLALCHEMY_SECRET_KEY'] = '123232'
+db = SQLAlchemy(app)
 
-@app.route('./')
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(127), nullable=False, unique=True)
+    massage = db.Column(db.text, nullable=False)
+    
+    def __str__(self):
+        return self.subject
+
+
+
+@app.route('/')
 def index():
     return 'Hello, Flask'
 
+@app.route('/contact',methods=['GET','POST',])
+def contact():
+    if request.methods == 'POST':
+        contact=Contact(
+            full_name =request.form.get('firstname'),
+            phone=request.form.get('phone'),
+            email =request.form.get('email'),
+            massage =request.form.get('massage')
+        )
+        db.session.add(contact)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template('contact.html')
 if __name__ == '__main__ ':
-    app.run(debage=True)
+    app.run(debug=True)
--- ProjectBackEnd/Flask/venv/Scripts/activate.bat
+++ ProjectBackEnd/Flask/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\TARIEL\Desktop\PragmatechFoundationProject-master\venv
+set VIRTUAL_ENV=C:\Users\TARIEL\Desktop\PragmatechFoundationProject-master\ProjectBackEnd\Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
